adefd7d94fe1769b31053d1ceac41a80ee149b55
Brian Rodriguez <brirod89@gmail.com>
2022-11-14 20:08:50 -0300

    Creacion de proyecto

---
+++ eCommerceSveltia.js
@@ -0,0 +1,62 @@
+let modelo = parseInt(
+    prompt(
+        "Escoger el producto que deseas comprar: 1. Criolipolisis - 2. Hammer Sveltia - 3. BodyGO Performance - 4. Dermolight Laser"
+        )
+)
+
+
+let totalCompra = 0
+let seguirComprando = true
+let decision
+
+
+class Equipo {
+    constructor(id, nombre, precio) {
+        this.id = id
+        this.nombre = nombre
+        this.precio = precio
+    }
+}
+
+const criolipolisis = new Equipo(1, "Criolipolisis", 600000)
+//precioLista.push(criolipolisis)
+const hammer = new Equipo(2, "Hammer Sveltia", 300000)
+//precioLista.push(hammer)
+const bodyGo = new Equipo(3, "BodyGo Perform", 1500000)
+//precioLista.push(bodyGo)
+const laser = new Equipo(4, "Dermolight Laser", 3500000)
+//precioLista.push(laser)
+
+const precioLista = [criolipolisis, hammer, bodyGo, laser]
+
+
+while (seguirComprando === true) {
+
+    const productoCliente = precioLista.find(producto => producto.id === modelo)
+
+    if (productoCliente) {
+        totalCompra = totalCompra + productoCliente.precio
+    } else {
+        modelo = parseInt(prompt(
+            "Escoger el producto que deseas comprar: 1. Criolipolisis - 2. Hammer Sveltia - 3. BodyGO Performance - 4. Dermolight Laser"
+        )
+        )
+
+        continue
+
+    }
+
+    decision = parseInt(prompt('Quieres seguir comprando? 1.Si - 2.No'))
+
+    if (decision === 1) {
+        producto = parseInt(
+            prompt(
+                "Escoger el producto que deseas comprar: 1. Criolipolisis - 2. Hammer Sveltia - 3. BodyGO Performance - 4. Dermolight Laser"
+            )
+        )
+    } else {
+        seguirComprando = false
+    }
+}
+
+alert(`El total de tu compra es ${totalCompra}`)
